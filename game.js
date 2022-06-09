7ecc4027192b4f13abcd2c45e3d037468d4e2b1f
Basil Ramsdell <bramsdell72@gmail.com>
2022-06-08 20:52:54 -0600

    First commit

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-
+const gameState = {
+    players: ['x', 'o'],
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ]
+  }
+  //must be able to add names to players
+  //have starting order random
+  //change turns
+  //can not place marker where marker exists
+  //check if last play was winning or draw, return proper value.
+    //check if 3 same values in a row
+    //check if 3 same values in a column
+    //check if 3 same values in diaganal directions
+//reset button
