5a38d8ee56885b5258bbe54d05815dec90755a0e
Basil Ramsdell <bramsdell72@gmail.com>
2022-06-10 09:35:33 -0600

    raw code no form

--- game.js
+++ game.js
@@ -15,6 +15,35 @@ const gameState = {
     //check if 3 same values in a column
     //check if 3 same values in diaganal directions
 //reset button
-for(let i = 0; i < gameState.board.length; i++){
-  let square = gameState.board[i];
+
+
+
+
+function checkWinner(){
+for(let i = 0; i < gameState.board.length; i++){ //loops through the 3 arrays
+  let row = gameState.board[i];
+    for(let k = 0; k < row.length; k++){//loops through the values inside the array
+      let square = row[k];
+      return square;
 }
+}
+/*Possible wins are
+rows
+[0,1,2] or
+[3,4,5] or
+[6,7,8]
+columns
+[0,3,6] or
+[1,4,7] or
+[2,5,8]
+diaganal
+[0,4,8] or
+[2,4,6]*/
+}
+
+let playBox = document.getElementsByClassName("box");
+playBox.addEventLisener('click', drawFunction()){
+  
+}
+
+
