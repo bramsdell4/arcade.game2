74dfefefd58375ee96c0140970599f0d28158dd8
Basil Ramsdell <bramsdell72@gmail.com>
2022-06-10 11:37:37 -0600

    more code

--- game.js
+++ game.js
@@ -1,10 +1,19 @@
+
+//-------this is a mess and I am sorry
+
+
+
+
+
+
 const gameState = {
     players: ['x', 'o'],
     board: [
       [null, null, null],
       [null, null, null],
       [null, null, null]
-    ]
+    ],
+    currentPlayer: [],
   }
   //must be able to add names to players
   //have starting order random
@@ -15,19 +24,24 @@ const gameState = {
     //check if 3 same values in a column
     //check if 3 same values in diaganal directions
 //reset button
-
-
-
+function playerTurn (){
+  for (let i = 0; i < gameState.players; i++){//loop through players
+    let current = gameState.players[i];
+    currentPlayer.push(current[i])//since its adding to the end of currentPlayer it probably also needs a function to remove for the next turn. maybe .unslice
+  }
+}
 
 function checkWinner(){
+  let newArr = []
 for(let i = 0; i < gameState.board.length; i++){ //loops through the 3 arrays
   let row = gameState.board[i];
     for(let k = 0; k < row.length; k++){//loops through the values inside the array
       let square = row[k];
-      return square;
+      newArr.push(square)//adds to new array so we can check for winners
 }
-}
-/*Possible wins are
+}//check if values are all the same in newArr and who owns said values
+/* if newArr equals any possible win game is over and owner of cards wins
+Possible wins are
 rows
 [0,1,2] or
 [3,4,5] or
@@ -42,7 +56,15 @@ diaganal
 }
 
 let playBox = document.getElementsByClassName("box");
-playBox.addEventLisener('click', drawFunction()){
+playBox.addEventLisener('click', drawFunction());//event listener that is supposed to display an X or O depeding on whose turn it is
+function drawFunction(){
+  if(gameState.players[0]){
+    playBox.innerText("X");// could this also be .innerHTML <p> X </p>
+  }
+  if(gameState.players[1]){
+    playBox.innerText("O")
+  }
+
   
 }
 
